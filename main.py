8eb7e745937db99cd2e55c45ff9fd737188e2d64
Rachel Schirra <schirra.rachel@gmail.com>
2019-06-25 18:48:04 -0700

    Changed ports for heroku because win10 pro is nicer than home

--- main.py
+++ main.py
@@ -55,5 +55,4 @@ def incomplete_tasks():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    host = os.environ.get("HOST", "127.0.0.1")
-    app.run(host=host, port=port)
+    app.run(host='0.0.0.0', port=port)
